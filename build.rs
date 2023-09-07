800599c60dcb0666d63be420d7e5eb14e02c2a20
chenxu.tian <472159011@qq.com>
2023-09-08 00:34:42 +0800

    🔥(cargo): Remove config
    Remove config

--- build.rs
+++ build.rs
@@ -2,9 +2,8 @@ fn main() -> Result<(), std::io::Error> {
     tonic_build::configure().build_server(true).compile(
         &[
             "proto/user.proto",
-            "proto/report.proto",
         ],
-        &["share_resources/proto"],
+        &["proto"],
     )?;
     Ok(())
 }
